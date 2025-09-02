3b393353ef5f868ad51b637dad407ab9c020b34c
ccoccooo <ccocco55@gmail.com>
2025-09-02 16:40:20 +0900

    update

--- src/test/java/com/example/youeatieat/mapper/AdminCalculateTests.java
+++ src/test/java/com/example/youeatieat/mapper/AdminCalculateTests.java
@@ -11,23 +11,23 @@ import org.springframework.boot.test.context.SpringBootTest;
 @Slf4j
 public class AdminCalculateTests {
 
-    @Autowired
-    private AdminCustomerMapper adminCustomerMapper;
-
-    @Test
-    void testPaymentCalc() {
-        Long memberId = 54L;
-
-        PaymentCalculateDTO result = adminCustomerMapper.selectPaymentCalculate(54L);
-
-        if (result == null) {
-            log.info("-------------memberId={} 의 결제 없당", memberId);
-            return;
-        }
-
-        log.info("최근 결제일 = {}", result.getLastPaymentDate());
-        log.info("총 결제금액 = {}", result.getTotalPrice());
-        log.info("총 주문수 = {}", result.getTotalOrders());
-    }
+//    @Autowired
+//    private AdminCustomerMapper adminCustomerMapper;
+//
+//    @Test
+//    void testPaymentCalc() {
+//        Long memberId = 54L;
+//
+//        PaymentCalculateDTO result = adminCustomerMapper.selectPaymentCalculate(54L);
+//
+//        if (result == null) {
+//            log.info("-------------memberId={} 의 결제 없당", memberId);
+//            return;
+//        }
+//
+//        log.info("최근 결제일 = {}", result.getLastPaymentDate());
+//        log.info("총 결제금액 = {}", result.getTotalPrice());
+//        log.info("총 주문수 = {}", result.getTotalOrders());
+//    }
 
 }
